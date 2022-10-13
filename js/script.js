d11cddee82fa34c30df3d337187785c63a3eab83
Nathan Schreiber <nathanschreiber76@gmail.com>
2022-10-13 13:34:15 -0400

    Add additional validation styles
    Added styles for required inputs while empty when submit is pressed.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
+const form = document.querySelector('form');
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
 const email = document.getElementById('email');
+const phone = document.getElementById('phone');
 const password = document.getElementById('password');
 const confirm = document.getElementById('confirm');
-const form = document.querySelector("form");
+const submit = document.getElementById('submit');
 
 // Validate field while typing
 email.addEventListener("input", (event) => {
@@ -14,9 +18,68 @@ email.addEventListener("input", (event) => {
     }
 });
 
+phone.addEventListener("input", (event) => {
+    if (phone.validity.valueMissing) {
+        phone.classList.remove("error");
+    } else if (!phone.validity.valid) {
+        phone.classList.add("error");
+    } else if (phone.validity.valid) {
+        phone.classList.remove("error");
+    }
+});
+
+firstName.addEventListener("input", (event) => {
+    if (firstName.value !== "") {
+        firstName.classList.remove("error");
+    }
+});
+
+lastName.addEventListener("input", (event) => {
+    if (lastName.value !== "") {
+        lastName.classList.remove("error");
+    }
+});
+
+password.addEventListener("input", (event) => {
+    if (password.value !== "") {
+        password.classList.remove("error");
+    }
+});
+
+confirm.addEventListener("input", (event) => {
+    if (confirm.value !== "") {
+        confirm.classList.remove("error");
+    }
+});
+
+
+// Check if password confirmation matches password
 form.addEventListener("submit", (event) => {
     if (confirm.value !== password.value) {
         event.preventDefault();
+        confirm.classList.add("error");
+    };
+});
+
+// Add error styles if fields are empty
+submit.addEventListener("click", (event) => {
+    if (firstName.validity.valueMissing) {
+        firstName.classList.add("error");
+    }
+
+    if (lastName.validity.valueMissing) {
+        lastName.classList.add("error");
+    }
+
+    if (email.validity.valueMissing) {
+        email.classList.add("error");
+    }
+
+    if (password.validity.valueMissing) {
+        password.classList.add("error");
+    }
+
+    if (confirm.validity.valueMissing) {
         confirm.classList.add("error");
     }
 });
